3e0a699b98bde9e37d37637fd332aa2af579f83c
Andrew Heyer <andrewbheyer@gmail.com>
2021-10-04 21:47:21 -0700

    adding material ui - issues with pagination positioning

--- src/pages/Users.tsx
+++ src/pages/Users.tsx
@@ -2,14 +2,18 @@ import React, {useEffect, useState} from 'react';
 import Layout from "../components/Layout";
 import axios from "axios";
 import {User} from "../models/user";
+import {Table, TableCell, TableHead, TableRow, TableBody, TablePagination, TableFooter} from "@mui/material";
+
 
 const Users = () => {
 
     const [users, setUsers] = useState<User[]>([]);
+    const [page, setPage] = useState(0);
+    const perPage = 10;
 
     useEffect(() => {
         (
-            async() => {
+            async () => {
                 const {data} = await axios.get('ambassadors')
 
                 setUsers(data);
@@ -19,30 +23,38 @@ const Users = () => {
 
     return (
         <Layout>
-            <table className="table table-striped table-sm">
-                <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Email</th>
-                    <th scope="col">Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {users.map(user => {
-                    return (
-                        <tr>
-                            <td>{user.id}</td>
-                            <td>{user.first_name}</td>
-                            <td>{user.email}</td>
-                            <td></td>
-                        </tr>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>#</TableCell>
+                        <TableCell>Name</TableCell>
+                        <TableCell>Email</TableCell>
+                        <TableCell>Actions</TableCell>
+                    </TableRow>
+                </TableHead>
 
-                    )
-                })}
-
-                </tbody>
-            </table>
+                <TableBody>
+                    {users.slice(page * perPage, (page + 1) * perPage).map(user => {
+                        return (
+                            <TableRow key={user.id}>
+                                <TableCell>{user.id}</TableCell>
+                                <TableCell>{user.first_name}</TableCell>
+                                <TableCell>{user.email}</TableCell>
+                                <TableCell></TableCell>
+                            </TableRow>
+                        )
+                    })}
+                </TableBody>
+                <TableFooter>
+                    <TablePagination
+                        count={users.length}
+                        page={page}
+                        onPageChange={(e, newPage) => setPage(newPage)}
+                        rowsPerPage={perPage}
+                        rowsPerPageOptions={[]}
+                    />
+                </TableFooter>
+            </Table>
         </Layout>
     );
 };
